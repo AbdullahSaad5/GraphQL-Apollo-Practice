08141b7110bf348d2414366783b904efc2f95315
AbdullahSaad5 <syedabdullahsaad1@gmail.com>
2022-08-21 23:52:24 +0500

    Apollo Server with GraphQL working on local fake data

---
+++ FakeData.js
@@ -0,0 +1,32 @@
+const UserList = [
+  {
+    id: 1,
+    name: "John",
+    username: "john",
+    age: 20,
+    nationality: "Canada",
+  },
+  {
+    id: 2,
+    name: "Pedro",
+    username: "PedroTech",
+    age: 20,
+    nationality: "Brazil",
+  },
+  {
+    id: 1,
+    name: "Sarah",
+    username: "cameron",
+    age: 25,
+    nationality: "United States",
+  },
+  {
+    id: 1,
+    name: "Rafe",
+    username: "rafe123",
+    age: 60,
+    nationality: "Germany",
+  },
+];
+
+module.exports = { UserList };

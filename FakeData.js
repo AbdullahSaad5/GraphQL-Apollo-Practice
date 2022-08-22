b9c282365a2bf46ab45d1d714665c86fc05cecb1
AbdullahSaad5 <syedabdullahsaad1@gmail.com>
2022-08-22 12:39:38 +0500

    Added Movies Data and implemented more queires

--- FakeData.js
+++ FakeData.js
@@ -4,29 +4,89 @@ const UserList = [
     name: "John",
     username: "john",
     age: 20,
-    nationality: "Canada",
+    nationality: "CANADA",
+    friends: [
+      {
+        id: 2,
+        name: "Pedro",
+        username: "PedroTech",
+        age: 20,
+        nationality: "BRAZIL",
+      },
+    ],
   },
   {
     id: 2,
     name: "Pedro",
     username: "PedroTech",
     age: 20,
-    nationality: "Brazil",
+    nationality: "BRAZIL",
+    friends: [
+      {
+        id: 4,
+        name: "Rafe",
+        username: "rafe123",
+        age: 60,
+        nationality: "GERMANY",
+      },
+    ],
   },
   {
-    id: 1,
+    id: 3,
     name: "Sarah",
     username: "cameron",
     age: 25,
-    nationality: "United States",
+    nationality: "INDIA",
   },
   {
-    id: 1,
+    id: 4,
     name: "Rafe",
     username: "rafe123",
     age: 60,
-    nationality: "Germany",
+    nationality: "GERMANY",
+    friends: [
+      {
+        id: 2,
+        name: "Pedro",
+        username: "PedroTech",
+        age: 20,
+        nationality: "BRAZIL",
+      },
+    ],
   },
 ];
 
-module.exports = { UserList };
+const MovieList = [
+  {
+    id: 1,
+    name: "Avengers Endgame",
+    yearOfPublication: 2019,
+    isInTheaters: true,
+  },
+  {
+    id: 2,
+    name: "Pursuit of Happyness ",
+    yearOfPublication: 1998,
+    isInTheaters: true,
+  },
+  {
+    id: 3,
+    name: "Interstellar",
+    yearOfPublication: 2007,
+    isInTheaters: true,
+  },
+  {
+    id: 4,
+    name: "Superbad",
+    yearOfPublication: 2009,
+    isInTheaters: false,
+  },
+  {
+    id: 5,
+    name: "Forrest Gump",
+    yearOfPublication: 2001,
+    isInTheaters: false,
+  },
+];
+
+module.exports = { UserList, MovieList };
--- schema/resolvers.js
+++ schema/resolvers.js
@@ -1,9 +1,28 @@
-const { UserList } = require("../FakeData");
+const { UserList, MovieList } = require("../FakeData");
 
 const resolvers = {
   Query: {
-    users() {
+    users: () => {
       return UserList;
+    },
+    user: (_, args) => {
+      const { id } = args;
+      const user = UserList.find((users) => users.id === Number(id));
+      return user;
+    },
+    movies: () => {
+      return MovieList;
+    },
+    movie: (_, args) => {
+      const { name } = args;
+      const movie = MovieList.find((movies) => movies.name === name);
+      return movie;
+    },
+  },
+
+  User: {
+    favoriteMovies: () => {
+      return MovieList.filter((movies) => movies.yearOfPublication >= 2009);
     },
   },
 };

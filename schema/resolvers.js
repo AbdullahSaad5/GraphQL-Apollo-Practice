5ee5e258b7272f5eccb0585ed56e522e057110f9
AbdullahSaad5 <syedabdullahsaad1@gmail.com>
2022-08-22 16:29:52 +0500

    Added Mutations of adding, updating and deeleting

--- schema/resolvers.js
+++ schema/resolvers.js
@@ -1,4 +1,4 @@
-const { UserList, MovieList } = require("../FakeData");
+let { UserList, MovieList } = require("../FakeData");
 
 const resolvers = {
   Query: {
@@ -17,6 +17,31 @@ const resolvers = {
       const { name } = args;
       const movie = MovieList.find((movies) => movies.name === name);
       return movie;
+    },
+  },
+
+  Mutation: {
+    createUser: (_, args) => {
+      let user = args.input;
+      const id = UserList[UserList.length - 1].id + 1;
+      user = { id, ...user };
+      UserList.push(user);
+      return user;
+    },
+    updateUsername: (_, args) => {
+      const { id, username } = args.input;
+      let updateUser;
+      UserList.map((user) => {
+        if (user.id == id) {
+          user.username = username;
+          updateUser = user;
+        }
+      });
+      return updateUser;
+    },
+    deleteUser: (_, args) => {
+      const { id } = args;
+      UserList = UserList.filter((user) => user.id !== id);
     },
   },
 
--- schema/type-defs.js
+++ schema/type-defs.js
@@ -25,11 +25,30 @@ const typeDefs = gql`
     movie(name: String!): Movie!
   }
 
+  type Mutation {
+    createUser(input: CreateUserInput!): User!
+    updateUsername(input: UpdateUsername!): User
+    deleteUser(id: ID!): User
+  }
+
+  input CreateUserInput {
+    name: String!
+    username: String!
+    age: Int!
+    nationality: Nationality
+  }
+
+  input UpdateUsername {
+    id: ID!
+    username: String!
+  }
+
   enum Nationality {
     CANADA
     INDIA
     BRAZIL
     GERMANY
+    PAKISTAN
   }
 `;
 
